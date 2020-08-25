6a3d9752c8d7da63b4448e9ac6521dc3a48d239d
James Stryker Mendez <Jamessmendez@gmail.com>
2020-08-25 12:46:16 -0400

    changed js file to display time

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-var yourName = "James";
-var year = new Date().getFullYear();
+var greeting = "";
+var dt = new Date();
+var h = dt.getHours(),
+  m = dt.getMinutes();
+var time = h > 12 ? h - 12 + ":" + m + " PM" : h + ":" + m + " AM";
+//day
+if (h < 12) {
+  greeting = "Good Morning";
+}
+//afternoon
+else if (h < 18) {
+  greeting = "Good Afternoon";
+}
+//night
+else {
+  greeting = "Good Night";
+}
 
 ReactDom.render(
-  <div className="App">
-    <p className="heading">{yourName}</p>
-    <p>{year}</p>
+  <div>
+    <h1>{greeting}</h1>
+    <h2>It is now {time}</h2>
   </div>,
   document.getElementById("root")
 );
